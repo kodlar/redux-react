a67bd2e0d6c9939d7e61b43437dba1f34afeabde
kodlar <ogankeskiner@gmail.com>
2016-11-08 01:08:51 +0200

    istisnai hatalar giderildi

--- project-imgurapi-middleware exercise/reducers/index.js
+++ project-imgurapi-middleware exercise/reducers/index.js
@@ -8,7 +8,7 @@ function combineReducer(currentState, action) {
   var nextState = Object.assign({},currentState);
   nextState  = {
 	  count : counter(nextState.count , action),
-	  sum : sum(nextState.sum, action)
+	  sum : sum(nextState.sum, action),
 	  images : images(nextState.images, action)
   }	
 	return nextState;
